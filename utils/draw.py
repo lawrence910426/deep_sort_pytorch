0a73cd62773982e6819264a644dc83b253a020f6
Lawrence Wu <lawrencewu@KKCompanydeMacBook-Air.local>
2022-08-30 14:24:04 +0800

    fix: cuda bugs

--- utils/draw.py
+++ utils/draw.py
@@ -2,7 +2,8 @@ from turtle import back
 import numpy as np
 import cv2
 from utils.shapes import Box, Line
-from numba import njit, prange
+from numba import cuda
+import math
 
 palette = (2 ** 11 - 1, 2 ** 15 - 1, 2 ** 20 - 1)
 
@@ -56,21 +57,29 @@ def draw_detector(background, detector: Line):
 @cuda.jit
 def render_detector(background, foreground, x1, y1, x2, y2):
     u1, u2 = x2 - x1, y2 - y1
-    length = np.sqrt(u1 ** 2 + u2 ** 2)
+    length = math.sqrt(u1 ** 2 + u2 ** 2)
     v1, v2 = u2 / length * 30, -u1 / length * 30
     det = u1 * v2 - u2 * v1
 
-    for i in range(background.shape[0]):
-        for j in range(background.shape[1]):
-            for col in range(3):
-                # since u and v are orthogonal, det must not be 0.
-                I, J = i - x1, j - y1
-                A, B = (v2 * I - v1 * J) / det, (-u2 * I + u1 * J) / det
-                x, y = int(A * foreground.shape[0]), int(B * foreground.shape[1])
-                alpha_foreground = foreground[x, y, 3] / 255.0
-                if 0 <= x <= foreground.shape[0] - 1 and 0 <= y <= foreground.shape[1] - 1:
-                    background[i, j, col] = alpha_foreground * foreground[x, y, col] + \
-                        background[i, j, col] * (1 - alpha_foreground)
+    tx = cuda.threadIdx.x
+    ty = cuda.blockIdx.x
+    bw = cuda.blockDim.x
+    gw = cuda.gridDim.x
+    pos = tx + ty * bw
+
+    for index in range(pos, background.shape[0] * background.shape[1] * 3, gw * bw):
+        i = index % background.shape[0]
+        j = (index // background.shape[0]) % background.shape[1]
+        col = (index // background.shape[0] // background.shape[1]) % 3
+        
+        # since u and v are orthogonal, det must not be 0.
+        I, J = i - x1, j - y1
+        A, B = (v2 * I - v1 * J) / det, (-u2 * I + u1 * J) / det
+        x, y = int(A * foreground.shape[0]), int(B * foreground.shape[1])
+        alpha_foreground = foreground[x, y, 3] / 255.0
+        if 0 <= x <= foreground.shape[0] - 1 and 0 <= y <= foreground.shape[1] - 1:
+            background[i, j, col] = alpha_foreground * foreground[x, y, col] + \
+                background[i, j, col] * (1 - alpha_foreground)
 
 if __name__ == '__main__':
     for i in range(82):
